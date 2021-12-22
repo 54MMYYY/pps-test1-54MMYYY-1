7a24d8498fd422b5100fda5c4a9c6f51de701666
Samson <95264901+54MMYYY@users.noreply.github.com>
2021-12-22 08:33:26 +0000

    ..

--- p3original.c
+++ p3original.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 void input(int *a, int *b)
 {
-
+  printf("Enter 2 numbers: ");
+  scanf("&d%d",&a,&b);
 }
-void add(int a, int b, int *sum)
+void add(int a, int b, int *c)
 {
-
+  *sum=a+b;
 }
-void output(int a, int b, int sum)
+void output(int a, int b, int c)
 {
-
+  
 }
+
